ba17c4db5301ff77185efcbd6f737f9fa04649ee
NekoSina <nekosalad@protonmail.com>
2022-03-22 13:00:21 +0430

    threading down. now i use 100 threads. wps =10-24

--- Program.cs
+++ Program.cs
@@ -4,24 +4,28 @@ using System.Net;
 
 public class DotNutBuster
 {
-    public static string wordlist_path = "sampleWordList.txt";
+    public static string wordlist_path = "/opt/passlist/directory-list-2.3-small.txt";
     public static string domain = "her.st";
-    public static Thread  newThread = new Thread(Bust);
+    public static Thread[]  bustingThreads = new Thread[100];
+    public static object SyncRoot = new object();
     public static Queue<string> dirs = new Queue<string>();
     public static List<string> found_directories = new List<string>();
     public static int url_count;
     //performance monitoring properties:
     public static bool verbious = true;
     public static Stopwatch wps_stopwatch = new Stopwatch();
-    public static Stopwatch main_stopwatch = new Stopwatch();
+    //public static Stopwatch main_stopwatch = new Stopwatch();
     public static Dictionary<string, string> testedUrls = new Dictionary<string, string>();
     public static void Main()
     {
         ReadWordlist();
-        newThread.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            bustingThreads[i] = new Thread (()=>Bust());
+            bustingThreads[i].Start();   
+        }
         wps_stopwatch.Start();
-        main_stopwatch.Start();
-        Thread.Sleep(1000);
+        //main_stopwatch.Start();
         Bust();
         PrintResults();
     }
@@ -37,47 +41,58 @@ public class DotNutBuster
     {
         WebClient client = new WebClient();
         string url = "";
-        while (dirs.Count > 0)
+        int count;
+        lock(SyncRoot)
+        {
+            count = dirs.Count;
+        }
+        while (count > 0)
         {
             try
             {
-                url = dirs.Dequeue();
+                lock(SyncRoot)
+	                url = dirs.Dequeue();
                 client.DownloadString(url);
                 Console.WriteLine(url);
-                found_directories.Add(url);
-                testedUrls.Add("valid url", url);
+                lock(SyncRoot)
+                    found_directories.Add(url);
             }
             catch (WebException ex)
             {
-                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
-                {
-                    testedUrls.TryAdd((ex.Message), url);
-                    if(verbious == true)
-                    {
-                        var resp = (HttpWebResponse)ex.Response;
-                        if (resp.StatusCode == HttpStatusCode.NotFound)
-                        {
-                            Console.WriteLine($"{url} : {resp.StatusCode}");
-                        }
-                    }
-                }
+                // if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
+                // {
+                //     testedUrls.TryAdd((ex.Message), url);
+                //     if(verbious == true)
+                //     {
+                //         var resp = (HttpWebResponse)ex.Response;
+                //         if (resp.StatusCode == HttpStatusCode.NotFound)
+                //         {
+                //             Console.WriteLine($"{url} : {resp.StatusCode}");
+                //         }
+                //     }
+                // }
 
             }
             finally
             {
-                if (wps_stopwatch.Elapsed.TotalSeconds > 10)
+                lock(SyncRoot)
                 {
-                    var newCount = dirs.Count;
-                    Console.WriteLine($"WPS { ((url_count - newCount) / wps_stopwatch.Elapsed.TotalSeconds).ToString("0.00") }");
-                    wps_stopwatch.Restart();
-                    url_count = newCount;
+                    count = dirs.Count;
+                    testedUrls.Add(url, "valid url");
+                    if (wps_stopwatch.Elapsed.TotalSeconds > 10)
+                    {
+                        var newCount = dirs.Count;
+                        Console.WriteLine($"WPS { ((url_count - newCount) / wps_stopwatch.Elapsed.TotalSeconds).ToString("0.00") }");
+                        wps_stopwatch.Restart();
+                        url_count = newCount;
+                    }
                 }
             }
         }
     }
     public static void PrintResults()
     {
-        Console.WriteLine($"total time :  {main_stopwatch.Elapsed.Seconds}");
+        //Console.WriteLine($"total time :  {main_stopwatch.Elapsed.Seconds}");
         Console.WriteLine("results:");
         foreach (var found_dir in found_directories)
         {
